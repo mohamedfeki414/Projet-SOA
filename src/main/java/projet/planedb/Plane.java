386d4ced2433956e200d652d17f4a1463fefe670
feki_mohamed <medfeki33@gmail.com>
2024-12-03 10:26:36 +0100

    Initial commit

---
+++ src/main/java/projet/planedb/Plane.java
@@ -0,0 +1,54 @@
+package projet.planedb;
+
+public class Plane {
+	private String numPlane;
+	private String villeDepart;
+	private String villeArrivee;
+	private int heureDepart;
+	
+	public Plane(String numPlane, String villeDepart,String villeArrivee,int heureDepart) {
+		
+		this.setNumPlane(numPlane);
+
+		this.setVilleDepart(villeDepart);
+
+		this.setVilleArrivee(villeArrivee);
+
+		this.setHeureDepart(heureDepart);
+		
+	}
+
+	public String getNumPlane() {
+		return numPlane;
+	}
+
+	public void setNumPlane(String numPlane) {
+		this.numPlane = numPlane;
+	}
+
+	public String getVilleDepart() {
+		return villeDepart;
+	}
+
+	public void setVilleDepart(String villeDepart) {
+		this.villeDepart = villeDepart;
+	}
+
+	public String getVilleArrivee() {
+		return villeArrivee;
+	}
+
+	public void setVilleArrivee(String villeArrivee) {
+		this.villeArrivee = villeArrivee;
+	}
+
+	public int getHeureDepart() {
+		return heureDepart;
+	}
+
+	public void setHeureDepart(int heureDepart) {
+		this.heureDepart = heureDepart;
+	}
+	
+ 
+}
